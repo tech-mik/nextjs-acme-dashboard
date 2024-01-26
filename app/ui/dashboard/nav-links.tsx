5a61d4d49ecc7272574fc6df35d56329078b2489
Mik ten Holt <miktenholt@gmail.com>
2024-01-26 21:23:53 +0100

    Removed dot again from menu

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -43,7 +43,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}.</p>
+            <p className="hidden md:block">{link.name}</p>
           </Link>
         );
       })}
