ccb5daac9bfe56bc201af7df428fce5716ae0580
Mik ten Holt <miktenholt@gmail.com>
2024-12-06 14:34:23 +0100

    Added login information to login field

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -35,6 +35,7 @@ export default function LoginForm() {
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
+                value="user@nextmail.com"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -54,6 +55,7 @@ export default function LoginForm() {
                 type="password"
                 name="password"
                 placeholder="Enter password"
+                value={'123456'}
                 required
                 minLength={6}
               />
